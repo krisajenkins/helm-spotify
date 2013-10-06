c058970895feac5d184da859998fc96a2ae03f6e
Kris Jenkins <krisajenkins@gmail.com>
2013-10-06 22:45:09 +0100

    Docstrings and packages header & footer.

--- helm-spotify.el
+++ helm-spotify.el
@@ -1,10 +1,28 @@
-;;; See: https://developer.spotify.com/technologies/web-api/
+;;; helm-spotify.el --- Control Spotify with Helm.
+;; Copyright 2013 Kris Jenkins
+;;
+;; Author: Kris Jenkins <krisajenkins@gmail.com>
+;; Maintainer: Kris Jenkins <krisajenkins@gmail.com>
+;; Keywords: helm spotify
+;; URL: https://github.com/krisajenkins/helm-spotify
+;; Created: 6th October 2013
+;; Version: 0.1.0
+;; Package-Requires: ((json "0.0.0") (s "0.0.0"))
+
+;;; Commentary:
+;;
+;; A search & play interface for Spotify.
+
+;;; Code:
+
+;;; API Reference: https://developer.spotify.com/technologies/web-api/
+(require 'helm)
 (require 'url)
 (require 'json)
 (require 's)
 
 (defun spotify-search (search-term)
-  "Search spotify for term, returning the results as a Lisp structure."
+  "Search spotify for SEARCH-TERM, returning the results as a Lisp structure."
   (let ((a-url (format "http://ws.spotify.com/search/1/track.json?q=%s" search-term)))
     (with-current-buffer
 	(url-retrieve-synchronously a-url)
@@ -13,7 +31,7 @@
       (json-read-object))))
 
 (defun spotify-format-track (track)
-  "Given a track, return a (\"Formatted name\" . <href>) pair."
+  "Given a TRACK, return a (\"Formatted name\" . <href>) pair."
   (let ((track-name (cdr (assoc 'name track)))
 	(album-name (cdr (assoc 'name (assoc 'album track))))
 	(artist-names (mapcar (lambda (artist)
@@ -31,17 +49,17 @@
 	    (cdr (assoc 'tracks results)))))
 
 (defun spotify-play-href (href)
-  "Get the Spotify app to play the object with the given href."
+  "Get the Spotify app to play the object with the given HREF."
   (shell-command (format "osascript -e 'tell application %S to play track %S'"
 			 "Spotify"
 			 href)))
 
 (defun spotify-play-track (track)
-  "Get the Spotify app to play the track."
+  "Get the Spotify app to play the TRACK."
   (spotify-play-href (cdr (assoc 'href track))))
 
 (defun spotify-play-album (track)
-  "Get the Spotify app to play the album for this track."
+  "Get the Spotify app to play the album for this TRACK."
   (spotify-play-href (cdr (assoc 'href (assoc 'album track)))))
 
 (defun helm-spotify-search ()
@@ -60,6 +78,10 @@
 
 ;;;###autoload
 (defun helm-spotify ()
+  "Bring up a Spotify search interface in helm."
   (interactive)
   (helm :sources '(helm-source-spotify-track-search)
 	:buffer "*helm-spotify*"))
+
+(provide 'helm-spotify)
+;;; helm-spotify.el ends here
