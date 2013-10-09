b154a47789af26d3d36e11e21368b2d67c730b81
Kris Jenkins <krisajenkins@gmail.com>
2013-10-09 21:26:13 +0100

    Adding some details about supporting other platforms.

--- helm-spotify.el
+++ helm-spotify.el
@@ -12,6 +12,10 @@
 ;;; Commentary:
 ;;
 ;; A search & play interface for Spotify.
+;;
+;; Currently supports OSX & Linux.
+;;
+;; (Want support for another platform? There's a guide in the github README.)
 
 ;;; Code:
 
